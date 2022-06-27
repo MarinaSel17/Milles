f8c2ecb3f43bf421d8df8d861365b6b26410b9ba
MarinaSel17 <aksinya919@mail.com>
2022-06-27 09:42:30 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        //входные данные
+        int amount = 12_000;
+        byte b = 20;
+        //логика программы
+        int bonus = amount / b;
+        System.out.println("Ваш бонус:" + bonus);
+
+    }
+}
